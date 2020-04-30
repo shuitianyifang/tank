350dd3c63c11f07522296e2ed4e6f22beb67dc32
shuitianyifang <40431070+shuitianyifang@users.noreply.github.com>
2020-04-30 19:52:37 +0800

    将坦克坐标定为变量，在paint()中改变值，paint()被调用后，方块就会改变位置

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -1,10 +1,6 @@
 package com.mashibing.tank;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class T {
+public class Main {
 
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -7,6 +7,8 @@ import java.awt.event.WindowEvent;
 // 继承Frame类
 public class TankFrame extends Frame {
 
+    int x = 200, y = 200;
+
     // 构造方法中初始化一些属性
     public TankFrame(){
         setSize(800,600);
@@ -23,11 +25,17 @@ public class TankFrame extends Frame {
         });
     }
 
-    // 重写paint方法,这个方法，在父类Container中
-    // Graphics相当于一支画笔，存在于系统中，它在 frame 中“画画”
+    /**
+     * 重写paint方法,这个方法，在父类Container中
+     * Graphics相当于一支画笔，存在于系统中，它在 frame 中“画画”
+     */
     @Override
     public void paint(Graphics g){
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50);
+
+        // 这样不断隐藏frame，点出frame，就会发现方块移动了
+        x += 10;
+        y += 10;
     }
 
 }
