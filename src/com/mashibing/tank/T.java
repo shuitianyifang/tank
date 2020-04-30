7bfe1fc065a59dfc5f7fb8aec4dd3615bdfa8bd6
shuitianyifang <40431070+shuitianyifang@users.noreply.github.com>
2020-04-30 19:46:05 +0800

    从Frame类继承，并且重写paint方法，画出一个黑方块

--- src/com/mashibing/tank/T.java
+++ src/com/mashibing/tank/T.java
@@ -7,18 +7,7 @@ import java.awt.event.WindowEvent;
 public class T {
 
     public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("tank war");
-        f.setVisible(true);
-
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        TankFrame tf = new TankFrame();
     }
 
 }
---
+++ src/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,33 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+// 继承Frame类
+public class TankFrame extends Frame {
+
+    // 构造方法中初始化一些属性
+    public TankFrame(){
+        setSize(800,600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        // 事件监听，点击小叉关闭窗口
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    // 重写paint方法,这个方法，在父类Container中
+    // Graphics相当于一支画笔，存在于系统中，它在 frame 中“画画”
+    @Override
+    public void paint(Graphics g){
+        g.fillRect(200,200,50,50);
+    }
+
+}
